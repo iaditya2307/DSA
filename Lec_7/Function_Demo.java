9e819c1200555bc18adf4139e77ac84f5571c1f0
iaditya2307 <96366246+iaditya2307@users.noreply.github.com>
2023-01-18 21:07:41 +0530

    Add files via upload

---
+++ Lec_7/Function_Demo.java
@@ -0,0 +1,30 @@
+package Lec_7;
+
+public class Function_Demo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        addition();
+
+    }
+
+    public static void addition() {
+
+        int a = 7;
+        int b = 8;
+        int c = a + b;
+        sub();
+        System.out.println(c);
+
+    }
+
+    public static void sub() {
+
+        int a = 7;
+        int b = 8;
+        int c = a - b;
+        System.out.println(c);
+
+    }
+
+}
