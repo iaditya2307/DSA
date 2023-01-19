cd74ce78178aa2f8a1381831cfe166ab8fc02386
iaditya2307 <96366246+iaditya2307@users.noreply.github.com>
2023-01-19 19:12:41 +0530

    Add files via upload

---
+++ Lec_10/Max_Value.java
@@ -0,0 +1,22 @@
+package Lec_10;
+
+public class Max_Value {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 1, 3, 2, 4, 422, -99};
+        System.out.println(max(arr));
+
+    }
+
+    public static int max(int[] arr) {
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+
+    }
+
+}
---
+++ Lec_10/Range_Reverse.java
@@ -0,0 +1,27 @@
+package Lec_10;
+
+public class Range_Reverse {
+
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 3, 5, 6, 1, 12, 134, 4, 5, 1, 3};
+        Reverse(arr, 2, 7);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+
+    }
+
+    public static void Reverse(int[] arr, int i, int j) {
+
+        while (i < j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+
+    }
+
+}
