1247804fe7c33d5e9e7a1023fe540bed1c143e82
iaditya2307 <96366246+iaditya2307@users.noreply.github.com>
2023-01-19 19:11:50 +0530

    Add files via upload

---
+++ Lec_9/Linear_Search.java
@@ -0,0 +1,22 @@
+package Lec_9;
+
+public class Linear_Search {
+
+    public static void main(String[] args) {
+        int[] arr = {2, 4, -1, 7, 3, 8, 9};
+        int item = 3;
+        System.out.println(Findindex(arr, item));
+
+    }
+
+    public static int Findindex(int[] arr, int item) {
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == item) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+}
