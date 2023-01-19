f6a483ab6cac0dcf5d892ccf9a330fa619835908
iaditya2307 <96366246+iaditya2307@users.noreply.github.com>
2023-01-19 19:10:47 +0530

    Add files via upload

---
+++ Lec_8/Arrays_Swap_Demo2.java
@@ -0,0 +1,18 @@
+package Lec_8;
+
+public class Arrays_Swap_Demo2 {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 6, -7};
+        System.out.println(arr[0] + " " + arr[1]);
+        swap(arr, 0, 1);
+        System.out.println(arr[0] + " " + arr[1]);
+
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
